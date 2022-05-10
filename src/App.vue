c448d5deb28e62a3b56999372400f6b8fd756fc7
Zsigil <chickpox@freemail.hu>
2022-05-10 15:10:19 +0200

    creating new component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <h2>My Vue 3 Project</h2>
+    <div id="nav" class="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+});
+</script>
 
 <style lang="scss">
 #app {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <ItemsListComponent :items="items" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+import ItemsListComponent from "@/components/items/ItemsList.component.vue";
 
 export default defineComponent({
-  name: 'HomeView',
+  name: "Home",
   components: {
-    HelloWorld,
+    ItemsListComponent,
+  },
+  setup() {
+    const items: any[] = [
+      {
+        id: 1,
+        name: "Item 1",
+      },
+      {
+        id: 2,
+        name: "Item 2",
+      },
+      {
+        id: 3,
+        name: "Item 3",
+      },
+    ];
+    return {
+      items,
+    };
   },
 });
 </script>
